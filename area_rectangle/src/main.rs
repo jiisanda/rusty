f39f20ed8e27697cd2dc513cf5e72582f9eced3d
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-05-20 16:30:00 +0530

    area using tuples

--- area_rectangle/src/main.rs
+++ area_rectangle/src/main.rs
@@ -1,15 +1,14 @@
 // View commits for this file to changes
 
 fn main() {
-    let width = 30;
-    let height = 50;
+    let rect = (30, 50);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width, height)
+        area(rect)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
