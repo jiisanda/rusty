f62555260f33c1662549195bc27d37210b50bf5f
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-05-20 16:27:00 +0530

    normal area

---
+++ area_rectangle/src/main.rs
@@ -0,0 +1,15 @@
+// View commits for this file to changes
+
+fn main() {
+    let width = 30;
+    let height = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width, height)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
