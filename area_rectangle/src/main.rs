e598e2c469e7dab2747bbb414981d9de22b348f6
Harsh Jaiswal <harshjaiswal2307@gmail.com>
2023-05-20 16:50:27 +0530

    Methods and Associated functions

--- area_rectangle/src/main.rs
+++ area_rectangle/src/main.rs
@@ -5,9 +5,22 @@ struct Rectangle {
     height: u32,
 }
 
-impl Rectangle {
+impl Rectangle {            // Methods
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {                // Associated Functions -> differ from methods as no &self
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size 
+        }
     }
 }
 
@@ -17,7 +30,23 @@ fn main() {
         height: 50
     };
 
+    let rect1 = Rectangle {
+        width: 20,
+        height: 40
+    };
+
+    let rect2 = Rectangle {
+        width: 40,
+        height: 60
+    };
+
+    let rect3 = Rectangle::square(25);
+
+    println!("rect can hold rect1: {}", rect.can_hold(&rect1));
+    println!("rect can hold rect2: {}", rect.can_hold(&rect2));
+
     println!("rectangle: {:#?}", rect);
+    println!("square: {:#?}", rect3);
 
     println!(
         "The area of the rectangle is {} square pixels.",
