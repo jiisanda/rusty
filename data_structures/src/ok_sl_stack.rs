e331c1fad9d10b83edead9392074d99d6953f37a
jiisanda <jiisanda@protonmail.com>
2024-03-07 00:42:35 +0530

    peek

--- data_structures/src/ok_sl_stack.rs
+++ data_structures/src/ok_sl_stack.rs
@@ -11,7 +11,7 @@ struct Node<T> {
 
 impl<T> List<T> {
     pub fn new() -> Self {
-        List { head: None }             // We dont write List<T> when we construct an instance of list 
+        List { head: None }             // We don't write List<T> when we construct an instance of list
     }
 
     pub fn push(&mut self, elem: T) {
@@ -37,6 +37,18 @@ impl<T> List<T> {
         })                                  // using closures:
         // Closures are anonymous functions with extra superpower
         // they can refer to local variables outside the closure
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn mut_peek(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
     }
 }
 
@@ -87,4 +99,25 @@ mod test {
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
     }
+}
+
+#[test]
+fn peek() {
+    let mut list  = List::new();
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.mut_peek(), None);
+
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.mut_peek(), Some(&mut 3));
+
+    list.mut_peek().map(|value| {
+        *value = 42
+    });
+
+    assert_eq!(list.peek(), Some(&42));
+    assert_eq!(list.pop(), Some(42));
 }
