fe8e30cc00322368ee8df12c20c275df67659879
jiisanda <jiisanda@protonmail.com>
2024-05-09 17:06:00 +0530

    strsplit - with bugs

---
+++ strsplit/src/lib.rs
@@ -0,0 +1,42 @@
+#![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
+
+pub struct StrSplit {
+    remainder: &str,
+    delimiter: &str,
+}
+
+impl StrSplit {
+    pub fn new(haystack: &str, delimiter: &str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
+    }
+}
+
+//  let x: StrSplit;
+// for part in x {
+// }                    -> this is what iterator allows us to do
+impl Iterator for StrSplit {
+    type Item = &str;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
+        } else if self.remainder.is_empty() {
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = &[];
+            Some(rest)
+        }
+    }
+}
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters = StrSplit::new(haystack, " ");
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"].into_iter());
+}
