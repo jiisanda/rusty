855a4bf183e16ac3ffda16d7f9d4534c54cd0516
jiisanda <jiisanda@protonmail.com>
2024-05-21 23:35:55 +0530

    fix bug

--- vecmac/src/lib.rs
+++ vecmac/src/lib.rs
@@ -3,10 +3,23 @@ macro_rules! vecmac {
     () => {
         Vec::new()
     };       // these are not argument but pattern
+    ($element:expr) => {{
+        let mut vs = Vec::new();
+        vs.push($llement);
+        vs
+    }};
 }
 
 #[test]
 fn empty_vec() {
     let x: Vec<u32> = vecmac![];
     assert!(x.is_empty());
+}
+
+#[test]
+fn single() {
+    let x: Vec<u32> = vecmac![42];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 1);
+    assert_eq!(x[0], 42);
 }
